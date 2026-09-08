8b9a23795ff3e7a62e186bd20d9b252b9634df30
ethandilley <ethandilley68@gmail.com>
2026-09-08 19:54:43 -0400

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "ok")
+		fmt.Fprintln(w, "hi")
 	})
 
 	fmt.Println("listening on :8080")
